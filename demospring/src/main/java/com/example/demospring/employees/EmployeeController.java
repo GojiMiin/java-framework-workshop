7159efbb6fbb30273a27087f0c367ecc11c8bb27
Supachet Arunsuksiri <supachet.a@kbtg.tech>
2021-09-06 14:51:45 +0700

    add more employee

--- demospring/src/main/java/com/example/demospring/employees/EmployeeController.java
+++ demospring/src/main/java/com/example/demospring/employees/EmployeeController.java
@@ -8,8 +8,14 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     public EmployeeResponse[] getAll() {
-        return new EmployeeResponse[] {
-                new EmployeeResponse()
-        };
+        EmployeeResponse employee01 = new EmployeeResponse();
+        employee01.setId(1);
+        employee01.setName("Supachet");
+
+        EmployeeResponse employee02 = new EmployeeResponse();
+        employee02.setId(2);
+        employee02.setName("Supachet 2");
+
+        return new EmployeeResponse[] {employee01, employee02};
     }
 }
