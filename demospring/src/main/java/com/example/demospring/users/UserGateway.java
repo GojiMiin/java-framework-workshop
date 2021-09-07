de96919cc20bd5bfc87c40a72c4fd9d7254de6e2
Supachet Arunsuksiri <supachet.a@kbtg.tech>
2021-09-07 16:23:27 +0700

    create Gateway and generate response

---
+++ demospring/src/main/java/com/example/demospring/users/UserGateway.java
@@ -0,0 +1,10 @@
+package com.example.demospring.users;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserGateway {
+    public UserResponse getUserById(int id) {
+        return null;
+    }
+}
