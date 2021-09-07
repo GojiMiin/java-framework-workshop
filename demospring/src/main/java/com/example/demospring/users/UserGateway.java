69f9cec4bb00c2f6164ea15d8c1eb4e789061ba0
Supachet Arunsuksiri <supachet.a@kbtg.tech>
2021-09-07 16:41:20 +0700

    use RestTemplate to call external api

--- demospring/src/main/java/com/example/demospring/users/UserGateway.java
+++ demospring/src/main/java/com/example/demospring/users/UserGateway.java
@@ -1,10 +1,22 @@
 package com.example.demospring.users;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Component
 public class UserGateway {
-    public UserResponse getUserById(int id) {
-        return null;
+
+    private RestTemplate restTemplate;
+    public Optional<UserResponse> getUserById(int id) {
+        String url = "https://jsonplaceholder.typicode.com/users/" + id;
+        try {
+            UserResponse response = restTemplate.getForObject(url, UserResponse.class);
+            return Optional.ofNullable(response);
+        } catch (RestClientException e) {
+            return Optional.empty();
+        }
     }
 }
