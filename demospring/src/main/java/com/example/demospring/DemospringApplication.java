867bf5ddc4e12cf315d6b6fddee023f5447b335d
Supachet Arunsuksiri <supachet.a@kbtg.tech>
2021-09-07 15:13:41 +0700

    initial data in springboot

--- demospring/src/main/java/com/example/demospring/DemospringApplication.java
+++ demospring/src/main/java/com/example/demospring/DemospringApplication.java
@@ -4,11 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class DemospringApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DemospringApplication.class, args);
+		ConfigurableApplicationContext context
+				= SpringApplication.run(DemospringApplication.class, args);
+		// Beans
+		String[] names = context.getBeanDefinitionNames();
+		for (String name : names) {
+			System.out.println(name);
+		}
+		Arrays.stream(names).forEach(System.out::println);
+
 		System.out.println(context.getBeanDefinitionCount());
 	}
 
